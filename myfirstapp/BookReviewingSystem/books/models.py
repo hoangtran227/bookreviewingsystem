086ac95f09c63ad6bb5c045241899b791bceca66
DEV-01 <hoangtran227@gmail.com>
2018-05-02 17:40:59 +0700

    Create DB

--- myfirstapp/BookReviewingSystem/books/models.py
+++ myfirstapp/BookReviewingSystem/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50,primary_key=True)
+    description = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=50)
+    page_number = models.IntegerField(default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
